bb5476b69182d1e1e641be86c969e640cbf11976
martijnv <martijnv@fabrique.nl>
2026-06-30 22:15:18 +0200

    refactor  card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,9 +7,17 @@ interface CardProps {
   onClick?: () => void
 }
 
+const baseClasses =
+  'relative col-span-2 flex flex-col gap-2 border-4 border-black rounded-[7px] bg-surface bg-cover p-2 aspect-[3/4] [&>*]:z-[1]'
+const centeredClasses = 'justify-center items-center'
+const defaultClasses = 'justify-end items-start'
+
 const Card = ({ children, centered = false, className = '', onClick }: CardProps) => {
   return (
-    <div className={`card ${centered ? 'card--centered' : ''} ${className}`} onClick={onClick}>
+    <div
+      className={`${baseClasses} ${centered ? centeredClasses : defaultClasses} ${className}`}
+      onClick={onClick}
+    >
       {children}
     </div>
   )
