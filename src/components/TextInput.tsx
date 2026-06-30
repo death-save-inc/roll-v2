46359402d493caed10cd2d170b127e494ca14129
martijnv <martijnv@fabrique.nl>
2026-06-30 22:43:46 +0200

    fixed textinput

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -6,21 +6,19 @@ interface TextInputProps {
   placeholder?: string
 }
 
+const inputClasses =
+  'block w-full text-3xl font-charted line-height-1 text-primary-inverse border-solid border-1 border-primary bg-surface-inverse shadow-[inset_-2px_2px_0_#000]'
+
 const TextInput = ({ name, id, value, onChange, placeholder }: TextInputProps) => {
   return (
-    <div className="text-input">
-      <label className="text-input__label" htmlFor={`${name}-${id}`}>
-        {name}
-      </label>
-      <input
-        className="text-input__field"
-        type="text"
-        id={`${name}-${id}`}
-        value={value}
-        placeholder={placeholder}
-        onChange={(e) => onChange(e.target.value)}
-      />
-    </div>
+    <input
+      className={inputClasses}
+      type="text"
+      id={`${name}-${id}`}
+      value={value}
+      placeholder={placeholder}
+      onChange={(e) => onChange(e.target.value)}
+    />
   )
 }
 
