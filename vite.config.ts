16127842cefa0fbcb736c609424b8dbafae0c73b
martijnv <martijnv@fabrique.nl>
2026-07-10 13:39:45 +0200

    removed unusued fonts and styles, removed sass dependency

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@assets': path.resolve(__dirname, './assets'),
-      '@scss': path.resolve(__dirname, './src/scss'),
     },
   },
 })
